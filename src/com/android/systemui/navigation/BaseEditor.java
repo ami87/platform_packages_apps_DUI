5a9a2855d037e4ce2d3ff48b8656ef0f5ddff47a
Shubham Singh <coolsks94@gmail.com>
2017-11-26 02:16:32 +0530

    Use intents from Extensions
    Change-Id: I6ed10c83276ffdec6ff42f09540330fc0c034db7

--- src/com/android/systemui/navigation/BaseEditor.java
+++ src/com/android/systemui/navigation/BaseEditor.java
@@ -47,9 +47,9 @@ import android.widget.Toast;
 public abstract class BaseEditor implements Editor {
 
     public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
-    public static final String INTENT_ACTION_EDIT_COMPONENT = "com.dirtyunicorns.tweaks.ActionPickerDialogActivity";
-    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.dirtyunicorns.tweaks.IconPickerActivity";
-    public static final String INTENT_ACTION_GALLERY_PICKER_COMPONENT = "com.dirtyunicorns.tweaks.IconPickerGallery";
+    public static final String INTENT_ACTION_EDIT_COMPONENT = "org.aospextended.extensions.ActionPickerDialogActivity";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "org.aospextended.extensions.IconPickerActivity";
+    public static final String INTENT_ACTION_GALLERY_PICKER_COMPONENT = "org.aospextended.extensions.IconPickerGallery";
     public static final String INTENT_ICON_PICKER = "intent_icon_picker";
     public static final String INTENT_GALLERY_PICKER = "intent_gallery_picker";
     public static final String INTENT_ACTION_PICKER = "intent_action_action_picker";
