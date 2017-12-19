cb928cc679cf21e01de2831f4aa6c5903dda9f49
ezio84 <brabus84@gmail.com>
2017-12-19 01:03:59 +0100

    Be sure to set buttons darkintensity when needed
    e.g. on Fling open Settings to have light navbar and dark fling logo,
    rotate the screen, logo should be dark but will be white.

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -400,6 +400,8 @@ public class SmartBarView extends BaseNavigationBar {
         // Update menu button in case the IME state has changed.
         setMenuVisibility(mShowMenu, true);
         setDisabledFlags(mDisabledFlags, true);
+
+        mBarTransitions.reapplyDarkIntensity();
     }
 
     @Override
